50e034eb25f9d08aeca050e561356e14ddf57e7c
ITSC <ITSC@test.com>
2023-04-30 16:26:58 +0900

    숙제!

--- src/section06/Array03.java
+++ src/section06/Array03.java
@@ -17,15 +17,8 @@ public class Array03 {
 		
 		// 확장 for문
 		for(String name : names) {
-			// for 1번반복 String name = names[0];
-			// for 2번반복 String name = names[1];
-			// for 3번반복 String name = names[2];
-			// for 4번반복 String name = names[3];
-			// for 5번반복 String name = names[4];
-			// for 6번반복 String name = names[5];
 			System.out.println(name);
 		}
-		
 		
 	}
 
