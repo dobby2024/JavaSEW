fff9b1b58e4a514aaaef02831b4d6c7ba6f6134c
ITSC <ITSC@test.com>
2023-05-06 14:38:41 +0900

    loop 숙제

--- src/section05/Loop05.java
+++ src/section05/Loop05.java
@@ -8,7 +8,7 @@ public class Loop05 {
 	public static void main(String[] args) {
 		
 		for(int i = 0; i < 7; i++) {
-			for(int j = 0; j < 7; j++) {
+			for(int j = 0; j < i+1; j++) {
 				System.out.print("*");
 			}
 			System.out.println();	// 개행
--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -25,13 +25,15 @@ package section05;
 ******* 
   
 4번
-   *
-  ***
- *****
-*******
- *****
-  ***
-   *
+
+			i 		j
+   *		0		
+  ***		1		  
+ *****		2		
+*******		3
+ *****		4	+	6 = 10 > 9 
+  ***		5	+	5, 6 = 10 > 9 
+   *		6	+	4, 5, 6 = 10 > 9
 
    ☆
    *
@@ -44,5 +46,31 @@ package section05;
  */
 
 public class LoopHomeWork {
+	public static void main(String[] args) {
 
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 7; j++) {
+
+				if (i < 7 / 2 + 1) {
+					if (i + j < 3) {
+						System.out.print(" ");
+					} else if (j - i > 3) {
+						System.out.print(" ");
+					} else {
+						System.out.print("*");
+					}
+				} else {
+					if (i - j > 3) {
+						System.out.print(" ");
+					} else if (i + j > 9) {
+						System.out.print(" ");
+					}else {
+						System.out.print("*");
+					}
+				}
+			}
+			System.out.println(); // 개행
+		}
+
+	}
 }
