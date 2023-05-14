ed22a53fb44c1126b79a092812e2ad1e629fd982
ITSC <ITSC@test.com>
2023-05-14 17:41:51 +0900

    다형성

---
+++ src/section11/access1/Bus.java
@@ -0,0 +1,19 @@
+package section11.access1;
+
+public class Bus extends Car{
+	
+	public Bus() {
+		wheel = 4;
+		color = "Green";
+	}
+
+	@Override
+	public void drive() {
+		System.out.println("승객을 태우고 운전 합니다.");
+	}
+	
+	public void btnBell() {
+		System.out.println("벨을 누릅니다.");
+	}
+	
+}
