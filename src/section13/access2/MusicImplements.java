9826fe6b5adfe356d23fea04b06d20f9e4dae53f
ITSC <ITSC@test.com>
2023-05-20 17:22:25 +0900

    익명클래스

---
+++ src/section13/access2/MusicImplements.java
@@ -0,0 +1,21 @@
+package section13.access2;
+
+public class MusicImplements implements Music{
+	
+	private String brand = "SM";
+
+	@Override
+	public void listen() {
+		System.out.println("신나기 노래를 들어요!");
+	}
+
+	@Override
+	public void sing() {
+		System.out.println("즐겁게 노래를 불러요!");
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+
+}
