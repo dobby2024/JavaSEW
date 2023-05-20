96932b46b06d66304d59ceaefe3df4f4f7597b62
ITSC <ITSC@test.com>
2023-05-20 15:02:26 +0900

    인터페이스

---
+++ src/section12/InterfaceClass01.java
@@ -0,0 +1,13 @@
+package section12;
+/*
+ * 인터페이스(interface)
+ *	추상메소들의 집합
+ *	변수선언을 해도 변수가 아닌 상수가 된다.
+ *	다중상속 가능
+ *	
+ *	객체 구조(뼈대)를 만들어 준다. 
+ * 
+ */
+public class InterfaceClass01 {
+
+}
---
+++ src/section12/access2/Player.java
@@ -0,0 +1,13 @@
+package section12.access2;
+
+public interface Player {
+	
+	public void pass();
+	
+	public void kick();
+	
+	public void shot();
+	
+	public void dribble();
+
+}
